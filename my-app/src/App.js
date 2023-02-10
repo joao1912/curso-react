d5c94cdf7515e0bc7151e7c174511aee8b21a588
joão <joaopedrot1912@gmail.com>
2023-02-10 14:21:47 -0300

    react aplicado

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ola, react</h1>
+      <p>meu primeiro App</p>
+    </div>
+  );
+}
+
+export default App;
